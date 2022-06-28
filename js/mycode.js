d3d364ae8912558511e30f1c0f27557dc4b74473
anhtai46 <anhtaibp46@gmail.com>
2022-06-28 22:54:58 +0700

    sale-shoes

---
+++ js/mycode.js
@@ -0,0 +1,7 @@
+/*Mycode */
+function Color_Background() {
+ 	document.getElementById("mydiv").style.background = "#f7f48d";	
+ }
+    function abc(){
+            alert("Shoesvn has received your information. We will contact you as soon as possible");
+}
